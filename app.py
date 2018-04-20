e6017b6857214f8bc0e95948c2d8709b6f99a308
Alejandro Guzman <a.guillermoguzman@gmail.com>
2018-04-20 00:32:02 -0400

    Working app with compose

--- app.py
+++ app.py
@@ -1,37 +1,45 @@
-import asyncio
-import uvloop
-from motor.motor_asyncio import AsyncIOMotorClient
 from sanic import Sanic
 from sanic.response import json
 
+from motor.motor_asyncio import AsyncIOMotorClient
 
+import asyncio
+import uvloop
+
+
+# Setup #
+# todo: learn about this line. Is is necessary
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
-
-mongo_connection = AsyncIOMotorClient('localhost')
-contacts = mongo_connection.sanic_motor_test_db.contacts
-
-
 app = Sanic(__name__)
+db = None
 
+@app.listener('before_server_start')
+async def setup_db(app, loop):
+    global db
+    client = AsyncIOMotorClient(host='mongodb', io_loop=loop)
+    db = client['motor_test']
+
+# Routes #
+@app.route("/ping")
+async def list(request):
+    return json('Here..')
 
 @app.route("/")
 async def list(request):
-    data = await contacts.find().to_list(20)
+    data = await db.contacts.find().to_list(20)
     for x in data:
         x['id'] = str(x['_id'])
         del x['_id']
 
     return json(data)
 
-
 @app.route("/new")
 async def new(request):
-    contact = request.json
-    insert = await contacts.insert_one(contact)
-    return json({"inserted_id": str(insert.inserted_id)})
+    # contact = request.json
+    insert = await db.contacts.insert_one({'name': 'alejandro'})
+    return json('Inserted.')
 
-
+# Entry point #
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
     app.run(host="0.0.0.0", port=8000, workers=3, debug=True)
