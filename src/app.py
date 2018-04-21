91691a2117dcd03070270727d18b01cf5b06f1af
Alejandro Guzman <a.guillermoguzman@gmail.com>
2018-04-21 10:19:23 -0400

    Users collection.

--- src/app.py
+++ src/app.py
@@ -19,10 +19,9 @@ import asyncio
 import socket
 
 from sanic import Sanic
-from sanic.response import json
+from sanic.response import json, text
 from motor.motor_asyncio import AsyncIOMotorClient
 import uvloop
-
 
 ###### Setup ######
 
@@ -31,6 +30,7 @@ asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 app = Sanic(__name__)
 db = None
+
 
 @app.listener('before_server_start')
 async def setup_db(app, loop):
@@ -47,20 +47,20 @@ async def list(request):
     host_ip = socket.gethostbyname(host_name)
     return json({'Hostname': host_name, 'HostIP': host_ip})
 
-@app.route("/")
+@app.route("/users", methods=['GET'])
 async def list(request):
-    data = await db.contacts.find().to_list(20)
+    data = await db['users'].find().to_list(20)
     for x in data:
         x['id'] = str(x['_id'])
         del x['_id']
 
     return json(data)
 
-@app.route("/new")
+@app.route("/user", methods=['POST'])
 async def new(request):
-    # contact = request.json
-    insert = await db.contacts.insert_one({'name': 'alejandro'})
-    return json('Inserted.')
+    insert = await db['users'].insert_one(request.json)
+
+    return text('Inserted.')
 
 
 ###### Entry point ######
