2e04bd0e22bedbf1b3fbdedc4080a99725f924fe
Alejandro Guzman <a.guillermoguzman@gmail.com>
2018-04-21 01:10:16 -0400

    Moved to package, added license

--- run.sh
+++ run.sh
@@ -1,6 +1,26 @@
 #!/usr/bin/env bash
 
 
+# Run dev environment with mongo instance
+function dev {
+    docker-compose \
+    --file docker-compose-dev.yml \
+    up --build
+}
+
+# Run sanic server with no backend DB
+function run_now {
+    docker container run \
+        --interactive \
+        --tty \
+        --rm \
+        --name sanic-motor \
+        --volume "$PWD":/usr/src/myapp \
+        --workdir /usr/src/myapp \
+        python:3 \
+        python src/app.py
+}
+
 function build {
     docker image build \
         --tag my-python-app \
